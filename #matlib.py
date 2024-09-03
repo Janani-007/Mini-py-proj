ec82ba00597ff9dab21a1ca8ea67c07e146a6f71
Jan <123250861+Janani-007@users.noreply.github.com>
2024-09-03 17:26:43 +0530

    Add files via upload

---
+++ #matlib.py
@@ -0,0 +1,10 @@
+#matlib
+#Fill in the blanks with the user's input.....literally any thing you want
+
+adj =input("Adjective= ")
+verb1 =input("Verb= ")
+verb2 =input("Another Verb= ")
+famous_person =input("Famous Person= ")
+Sentence=f"Learning is so {adj}!  Every new concept I grasp ignites a spark of curiosity within me, pushing me to {verb1} uncharted territories of knowledge.\
+This continuous journey of discovery inspires me to {verb2} innovative ideas and solutions, much like {famous_person}"
+print(Sentence)
